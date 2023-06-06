380baeb5af1de62c94873ffed2176aea068e699b
Sébastien VIAL <sebvi66@hotmail.fr>
2023-06-06 22:33:27 +0000

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -9,30 +9,30 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-  let
-    inherit (self) outputs;
-    forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
-    forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+      forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
 
-    mkNixos = modules: nixpkgs.lib.nixosSystem {
-      inherit modules;
-      specialArgs = { inherit inputs outputs; };
-    };
-    mkHome = modules: pkgs: home-manager.lib.homeManagerConfiguration {
-      inherit modules pkgs;
-      extraSpecialArgs = { inherit inputs outputs; };
-    };
-  in 
-  {
-    nixosModules = import ./modules/nixos { inherit inputs outputs; };
-    packages = forEachPkgs (pkgs: import ./pkgs { inherit pkgs; });
+      mkNixos = modules: nixpkgs.lib.nixosSystem {
+        inherit modules;
+        specialArgs = { inherit inputs outputs; };
+      };
+      mkHome = modules: pkgs: home-manager.lib.homeManagerConfiguration {
+        inherit modules pkgs;
+        extraSpecialArgs = { inherit inputs outputs; };
+      };
+    in 
+    {
+      nixosModules = import ./modules/nixos { inherit inputs outputs; };
+      packages = forEachPkgs (pkgs: import ./pkgs { inherit pkgs; });
 
-    nixosConfigurations = {
-      mizu = mkNixos [ ./hosts/mizu ];
+      nixosConfigurations = {
+        mizu = mkNixos [ ./hosts/mizu ];
+      };
+      homeConfigurations = {
+        "shyrogan@mizu" = mkHome [ ./home/shyrogan/mizu.nix ] nixpkgs.legacyPackages."x86_64-linux";
+      };
     };
-    homeConfigurations = {
-      "shyrogan@mizu" = mkHome [ ./home/shyrogan/mizu.nix ] nixpkgs.legacyPackages."x86_64-linux";
-    };
-  };
 }
