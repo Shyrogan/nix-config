7ce1b7d7b54e148da0de18181e7cd8be61589b84
Sébastien VIAL <sebvi66@hotmail.fr>
2023-06-06 22:30:04 +0000

    fix: flake.nix

--- flake.nix
+++ flake.nix
@@ -9,25 +9,24 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
-    let 
-      inherit (self) outputs;
-      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-      forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+  let 
+    inherit (self) outputs;
+    forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+    forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
 
-      mkNixos = modules: nixpkgs.lib.nixosSystem {
-        inherit modules;
-        specialArgs = { inherit inputs outputs; }
-      };
-      mkHome = modules: home-manager.lib.homeManagerConfiguration {
-        inherit modules pkgs;
-        extraSpecialArgs = { inherit inputs outputs };
-      };
-    in 
-    {
-      nixosModules = import ./modules/nixos { inherit inputs outputs; };
-      packages = forEachPkgs (pkgs: import ./pkgs { inherit pkgs; });
+    mkNixos = modules: nixpkgs.lib.nixosSystem {
+      inherit modules;
+      specialArgs = { inherit inputs outputs; }
     };
+    mkHome = modules: home-manager.lib.homeManagerConfiguration {
+      inherit modules pkgs;
+      extraSpecialArgs = { inherit inputs outputs };
+    };
+  in 
+  {
+    nixosModules = import ./modules/nixos { inherit inputs outputs; };
+    packages = forEachPkgs (pkgs: import ./pkgs { inherit pkgs; });
 
     nixosConfigurations = {
       mizu = mkNixos [ ./hosts/mizu ];
