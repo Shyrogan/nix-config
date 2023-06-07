a69a4411d565a92fa15811d80fa3a9e059dfe193
Sébastien VIAL <sebvi66@hotmail.fr>
2023-06-07 09:24:14 +0000

    fix: stateVersion

--- home/shyrogan/global.nix
+++ home/shyrogan/global.nix
@@ -28,7 +28,7 @@
   home = {
     username = lib.mkDefault "shyrogan";
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
-    stateVersion = lib.mkDefault "22.05";
+    stateVersion = lib.mkDefault "23.05";
     sessionPath = [ "$HOME/.local/bin" ];
 
     persistence = {
