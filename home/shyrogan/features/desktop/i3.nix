1d0de1750a2fb4571aa54734a88c760bd342e46c
Sébastien VIAL <sebvi66@hotmail.fr>
2023-06-07 10:40:54 +0000

    feat: i3

---
+++ home/shyrogan/features/desktop/i3.nix
@@ -0,0 +1,5 @@
+{ inputs, lib, config, pkgs, ... }: {
+  xsession.windowManager.i3 = {
+    enable = true;
+  };
+};
--- home/shyrogan/mizu.nix
+++ home/shyrogan/mizu.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./global.nix
+    ./features/desktop/i3.nix
   ];
 }
